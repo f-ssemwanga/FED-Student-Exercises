d66fd9bbc27c2ac8dd087a4506d9b33c255116ed
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-04-25 19:06:41 +0100

    Completed Week 1 exercises

---
+++ 3 - JS/Week 1/assets/js/exercise2.js
@@ -0,0 +1,22 @@
+
+/***************This is the start of my Exercise 2***************/
+// const num1 = 3
+// const num2 = 6
+
+//console.log(`The sum of ${num1} + ${num2} = ${num1+num2}`)
+
+/***************This is the start of my Exercise 2***************/
+
+
+/* Here I am tring to run this script from
+ *  within the browser - see index.html */
+
+
+const num1 = parseInt(prompt('Enter first number: '));
+const num2 = parseInt(prompt('Enter second number: '));
+
+console.log(`The sum of ${num1} + ${num2} = ${num1+num2}`);
+
+
+
+frm.addEventListener('submit')
